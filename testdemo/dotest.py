e0250d9fa83f9ccdfc7a091429e6f6d337fe95f3
charlessoft <charlessoft@qq.com>
2013-12-20 23:38:22 +0800

    多线程上传

--- testdemo/dotest.py
+++ testdemo/dotest.py
@@ -9,27 +9,47 @@ from thread_queue import *
 import uuid
 import string
 import random
+import shutil
 
 
 
-
-class BuildData(Producer):
+class ProductData(Producer):
+    m_count = 0
     def __init__(self, t_name, queue):
         threading.Thread.__init__(self, name = t_name )
         self.data = queue
     def run(self):
-        strfilepath = './temp/' + str(uuid.uuid1()) + '.txt'
-        self.buildData( strfilepath, random.randint(1,10) )
+        while(True):
+            if self.m_count >10 :
+                break
+            else:
+                self.m_count = self.m_count + 1
+                strfilepath = './temp/' + str(uuid.uuid1()) + '.txt'
+                self.buildData( strfilepath, random.randint(1,10) )
 
     def _GenreateData( self, length ):
         chars = string.ascii_letters + string.digits;
         return ''.join([random.choice(chars) for i in range(length)])
 
     def buildData( self, path, size ):
-        file = open( path, "wb" )
-        #file.seek( 1024*1024*size )
-        file.write( self._GenreateData(size*1024*1024) )
-        file.close()
+        shutil.copyfile( "./temp/1.dat", path )
+        self.data.put( path )
+        #file = open( path, "wb" )
+        ##file.seek( 1024*1024*size )
+        #file.write( self._GenreateData(size*1024*1024) )
+        #file.close()
+
+class ConsumerToUds(Consumer):
+    def __init__(self, t_name, udsTest, queue):
+        threading.Thread.__init__(self, name = t_name )
+        self.data = queue
+        self.udsInfo = udsTest
+    def run(self):
+        path = self.data.get()
+        if path.strip()!='':
+            self.SendData( path )
+    def SendData( self, path ):
+        self.udsInfo.do_upload( path )
 
 class udsTest( cmd.Cmd ):
     m_Host = ""
@@ -44,9 +64,9 @@ class udsTest( cmd.Cmd ):
     def do_hello( self, arg ):
         print "********************************************"
         print "Create by charlesoft"
-        print "cmd \"upload\" for upload"
+        print "cmd \"upload 1.txt\" for upload"
         print "cmd \"download\" for download"
-        print "cmd \"muldownload\" for multithreaded"
+        print "cmd \"mulupload\" for multi thread"
         print "********************************************"
 
     def do_help( self, arg ):
@@ -60,8 +80,15 @@ class udsTest( cmd.Cmd ):
         print 'input =', line
         self.do_hello( "" )
 
-    def do_muldownload():
-        print("s")
+    def do_mulupload( self, arg ):
+        queue = Queue()
+        for i in range(1):
+            proc= ProductData("pro",queue)
+            proc.start()
+        for i in range(5):
+            consumer = ConsumerToUds("con", self, queue)
+            consumer.start()
+        #queue.join()
 
     def do_test():
         udsDocumentMgr = UDSMgr( "10.142.49.238", "/http/document!execute", 7002 )
@@ -96,9 +123,9 @@ class udsTest( cmd.Cmd ):
         udsdocMgr.DownloadFile( strFormInfo, strProperty )
 
     def do_upload( self, arg ):
-        filename = "1.txt"
+        filename = arg #"1.txt"
         strFormInfo = "{\"userName\":\"admin\",\"sysCheckNo\":\"74D631A4DF157D87B5B123369ADE61B9\",\"method\":\"add\"}"
-        strProperty = "{\"object_type\":\"ecm_document\",\"file_type\":[\"%s\"],\"file_name\":[\"%s\"]}" %(os.path.splitext(filename)[1][1:], os.path.splitext(filename)[0])
+        strProperty = "{\"object_type\":\"ecm_document\",\"file_type\":[\"%s\"],\"file_name\":[\"%s\"]}" %(os.path.splitext(filename)[1][1:],os.path.basename(filename) )
         strfiles = "{\"uploadFileDTO.fileList\":\"%s\"}" %(filename)
         print strFormInfo
         print strProperty
@@ -110,11 +137,9 @@ class udsTest( cmd.Cmd ):
         #        "{\"object_type\":\"ecm_document\",\"file_type\":[\"txt\"],\"file_name\":[\"test\"]}",
         #        "{\"uploadFileDTO.fileList\":\"1.txt\"}")
 def main():
-    queue = Queue()
-    test1 = BuildData("prox",queue)
-    test1.start()
-    test1.join()
-    #term = udsTest()
-    #term.cmdloop()
+
+        #test1.join()
+    term = udsTest()
+    term.cmdloop()
 if __name__ == '__main__':
     main()
